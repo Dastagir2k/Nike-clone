6da7bd52ab1fd244e89727652f34ec187b158330
Dastagir2k <dastagir2k@gmail.com>
2023-09-25 13:54:50 +0530

    Customer Review   section is finished

---
+++ my-project/src/components/ReviewCard.jsx
@@ -0,0 +1,25 @@
+import { star } from "../assets/icons"
+
+
+const ReviewCard = ({imgURL,CustomerName,rating,feedback}) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+        <img
+        src={imgURL}
+        alt="customer"
+        className="rounded-full object-cover w-[120px] h-[120px]"/>
+        <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+        <div className="mt-3 flex justify-center items-center gap-2.5">
+            <img
+            src={star}
+            width={24}
+            height={24}
+            className="object-contain m-0"/>
+            <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
+        </div>
+        <h3 className="mt-1 font-palanquin text-center text-3xl font-bold">{CustomerName}</h3>
+    </div>
+  )
+}
+
+export default ReviewCard
